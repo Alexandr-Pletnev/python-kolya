5a317371f938f8bb1035ac1199167ff7be631a6b
alexandr.pletnev <pletnev.alexandr@gmail.com>
2021-11-21 18:27:42 +0300

    Blank site - version 1

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app.secret_key = "mega secret"
 def get_step_data(arr, i, step):
     data = type('obj', (object,), {})
     data.nextIndex = i + 1
-    data.nextStep = step + 1
+    data.nextStep = step + 1  # не может быть > 2
     data.array = arr
+    data.description = " Какое то описание ..."
     return data
 
 
@@ -31,28 +32,17 @@ def start():
     session["currentArray"] = arr
     return nextStep()
 
-    # data = do_step(arr, 0, 0)
-    #
-    # ctx = createContext()
-    # ctx.userInput = input
-    # ctx.array = data.array
-    # ctx.indx = data.nextIndex
-    # ctx.step = data.nextStep
-    # page = render_template("start.html", ctx=ctx)
-    # return page
-
-
 @app.route('/step')
 def nextStep():
     indx = int(request.args.get('indx', 0))
     step = int(request.args.get('step', 0))
     arr = session["currentArray"]
     data = get_step_data(arr, indx, step)
+
     ctx = type('obj', (object,), {})
     ctx.userInput = session["userInput"]
-    ctx.array = data.array
-    ctx.indx = data.nextIndex
-    ctx.step = data.nextStep
+    ctx.data = data
+
     page = render_template("step.html", ctx=ctx)
     return page
 
